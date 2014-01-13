65e3a433b1495a6b74d93d5e02dcae8f658aaa1d
Felix Schäfer <felix@fachschaften.org>
2014-01-13 17:48:26 +0100

    Support most of Windows-1258

--- lib/mail/version_specific/ruby_1_9.rb
+++ lib/mail/version_specific/ruby_1_9.rb
@@ -119,7 +119,12 @@ module Mail
 
       # Windows-1252 and alike
       when /Windows-?(.*)/i
-        "Windows-#{$1}"
+        # Ruby can't convert Windows-1258 to UTF-8, Windows-1252 is close enough for us
+        if $1 == "1258"
+          "Windows-1252"
+        else
+          "Windows-#{$1}"
+        end
 
       when /^8bit$/
         Encoding::ASCII_8BIT
