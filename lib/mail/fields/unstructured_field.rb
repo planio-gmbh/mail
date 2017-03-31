ccdfeb2c05b7262d242fcc3d2bafa8c923600bd7
Holger Just <holger@plan.io>
2017-03-31 12:29:57 +0200

    Don't die on invalid encodings on unstructured fields

--- lib/mail/fields/unstructured_field.rb
+++ lib/mail/fields/unstructured_field.rb
@@ -148,7 +148,7 @@ module Mail
         first_word = true
         while !words.empty?
           break unless word = words.first.dup
-          word.encode!(charset) if charset && word.respond_to?(:encode!)
+          word.encode!(charset, undef: :replace, invalid: :replace) if charset && word.respond_to?(:encode!)
           word = encode(word) if should_encode
           word = encode_crlf(word)
           # Skip to next line if we're going to go past the limit
