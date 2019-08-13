be7ec78e1d48085e8624ae93c82b46d54e49e5c0
Felix Schäfer <felix@plan.io>
2019-08-13 12:24:10 +0200

    Don't require Content-Disposition: attachment
    It doesn't seem to be mandatory and the original
    surrounding code doesn't check for it either

--- lib/mail/message.rb
+++ lib/mail/message.rb
@@ -2151,9 +2151,6 @@ module Mail
       end
 
       if filename.nil? and
-        disp = header[:content_disposition] and
-        disp.respond_to?(:disposition_type) and
-        disp.disposition_type == 'attachment' and
         header[:content_type].to_s =~ /^message\/rfc822/
         # invent a filename for nameless attached emails
         filename = (Mail.new(body).subject || "email") + '.eml'
